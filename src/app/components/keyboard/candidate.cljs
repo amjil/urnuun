50005e50c9c4e3e2ee7dad02a0ad4bff3aa80352
amjil <amjil@protonmail.com>
2021-03-15 18:05:25 +0800

    change color

--- src/app/components/keyboard/candidate.cljs
+++ src/app/components/keyboard/candidate.cljs
@@ -9,11 +9,11 @@
         total-record (count cands)
         total (- (int (/ total-record 9)) (if (zero? (mod total-record 9)) 1 0))]
     (if-not (empty? cands)
-      [:div.pt-5.z-10.overflow-x-auto.mode-lr.leading-4.bg-gray-50.rounded-xl.shadow-2xl.w-44.h-32.absolute
+      [:div.pt-5.z-10.overflow-x-auto.mode-lr.leading-4.border-purple-100.border.bg-gray-50.rounded-xl.shadow-2xl.w-44.h-32.absolute
        [:div.mx-2.my-2
         [:ol.list-decimal
          (for [x (take 9 (drop (* 9 cand-page) cands))]
            [:li {:ref (:id x)} (:char_word x)])]
         [:div.flex.flex-cel.justify-between
-         [:p {:class (if (= 0 cand-page) "" "text-gray-500")} "<"]
-         [:p {:class (if (> total cand-page) "" "text-gray-500")} ">"]]]])))
+         [:p {:class (if (= 0 cand-page) "" "text-gray-200")} "<"]
+         [:p {:class (if (> total cand-page) "" "text-gray-200")} ">"]]]])))
