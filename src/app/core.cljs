dbf4af31629404776721eb57a7e3db84da3ddfe1
amjil <amjil@protonmail.com>
2021-03-17 17:11:31 +0800

    add side bar

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -16,8 +16,14 @@
 
   []
   [:div.flex.flex-cel.min-h-screen
-   [:header.mode-lr "navigation bar"]
-   [:div.flex-grow
+   [:div.mode-lr.top-0.left-0.fixed.h-full.w-16 
+    [:div.py-5.translate-x-1.bg-white.border-opacity-100.border-r.border-gray-50.shadow-md.h-full.w-full.flex.flex-col.justify-center.overflow-hidden
+     [:a.hidden.md:flex.items-center.w-full.mb-auto
+      [:img.w-14.transform.rotate-90 {:src "/img/logo.svg"}]
+      [:div.text-base.font-thin.mt-4
+       [:span.text-xl.text-purple-700 "ᠶᠠᠭᠤ ᠪᠣᠢ "]
+       [:span.text-sm.text-gray-500 "ᠶᠠᠭᠤ ᠪᠣᠢ ᠶᠠᠭᠤ"]]]]]
+   [:div.flex-grow.md:pl-16
     [:div.bg-gradient-to-r.from-light-blue-50.to-light-blue-100
      [:h1 (:text @app-state)]
      [:h3 "Edit this and watch it change"]
